3adf3d0fb2b284098e4b10dfab5548bdc5ad2cf3
Joel Meng <joel.meng@tattsgroup.com>
2019-07-09 11:30:10 +1000

    Add DeepDiff to Reflex targe

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Reflex",
-            dependencies: []),
+            dependencies: ["DeepDiff"]),
         .testTarget(
             name: "ReflexTests",
             dependencies: ["Reflex"]),
